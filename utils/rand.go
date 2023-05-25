0cc8fbc0d309db9a768de4600740be81bf22df7a
Shingo Gnix <870656738@qq.com>
2023-05-25 23:14:40 +0800

    feat: utils for test

---
+++ utils/rand.go
@@ -0,0 +1,24 @@
+package utils
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+var (
+	randStr = rand.New(rand.NewSource(time.Now().Unix()))
+	letters = []byte("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+)
+
+func TestKey(i int) []byte {
+	return []byte(fmt.Sprintf("yojou-key-%09d", i))
+}
+
+func RandValue(n int) []byte {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letters[randStr.Intn(len(letters))]
+	}
+	return []byte("(^ =3^)" + string(b))
+}
