9b43b23cc8ab0d6353245e03ebd7aae6ee11d067
Shingo Gnix <870656738@qq.com>
2023-06-08 20:44:01 +0800

    refactor: package redis

--- redis/redis_test.go
+++ redis/redis_test.go
@@ -9,6 +9,32 @@ import (
 	"yojoudb/utils"
 )
 
+func TestRedis_Del_Type(t *testing.T) {
+	opts := yojoudb.DefaultOptions
+	dir, _ := os.MkdirTemp("", "yojoudb-redis-del-type")
+	opts.DirPath = dir
+	rds, err := NewRedisCmd(opts)
+	assert.Nil(t, err)
+
+	// del
+	err = rds.Del(utils.TestKey(11))
+	assert.Nil(t, err)
+
+	err = rds.Set(utils.TestKey(1), 0, utils.RandValue(100))
+	assert.Nil(t, err)
+
+	// type
+	typ, err := rds.Type(utils.TestKey(1))
+	assert.Nil(t, err)
+	assert.Equal(t, String, typ)
+
+	err = rds.Del(utils.TestKey(1))
+	assert.Nil(t, err)
+
+	_, err = rds.Get(utils.TestKey(1))
+	assert.Equal(t, yojoudb.ErrKeyNotFound, err)
+}
+
 func TestRedis_Set_Get(t *testing.T) {
 	opts := yojoudb.DefaultOptions
 	dir, _ := os.MkdirTemp("", "yojoudb-redis-set-get")
