57b3f90f0991b81407914d7d12c47ed61dedd26c
Shingo Gnix <870656738@qq.com>
2023-07-29 23:21:40 +0800

    feat: WAL reader

--- wal/wal.go
+++ wal/wal.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	lru "github.com/hashicorp/golang-lru/v2"
+	"io"
 	"os"
 	"sort"
 	"sync"
@@ -33,6 +34,15 @@ type WAL struct {
 	mu         sync.RWMutex
 	blockCache BlockCache
 	bytesWrite uint32
+}
+
+// Reader represents a reader for WAL.
+// The readers are *segmentReader for every segment,
+// sorted by segment id.
+// And the currIdx points to current segment reader.
+type Reader struct {
+	readers []*segmentReader
+	currIdx int
 }
 
 func Open(opt Options) (*WAL, error) {
@@ -106,6 +116,25 @@ func Open(opt Options) (*WAL, error) {
 	return wal, nil
 }
 
+// OpenNewActiveSeg opens a new segment file and sets it
+// as the active segment file regardless of the old one.
+// Calling it in merge process.
+func (w *WAL) OpenNewActiveSeg() error {
+	w.mu.Lock()
+	defer w.mu.Unlock()
+	if err := w.activeSeg.Sync(); err != nil {
+		return err
+	}
+	seg, err := openSegmentFile(w.options.DirPath,
+		w.options.SegmentFileExt, w.activeSeg.id+1, w.blockCache)
+	if err != nil {
+		return err
+	}
+	w.olderSegs[w.activeSeg.id] = w.activeSeg
+	w.activeSeg = seg
+	return nil
+}
+
 // Write writes the data to the WAL.
 // Actually, it writes the data to the active segment file.
 // Returns the location of the data in the WAL.
@@ -176,6 +205,109 @@ func (w *WAL) Read(loc *ChunkLoc) ([]byte, error) {
 	return seg.Read(loc.BlockIndex, loc.ChunkOffset)
 }
 
+// NewReaderLE returns a new reader for WAL which only read
+// data from the segment whose id is less than or equal to
+// the given segId.
+// If segId is 0, meaning read from all segments.
+// You may also call it in the merge process.
+func (w *WAL) NewReaderLE(segId SegmentID) *Reader {
+	w.mu.RLock()
+	defer w.mu.RUnlock()
+
+	var readers []*segmentReader
+	for _, seg := range w.olderSegs {
+		if segId == 0 || seg.id <= segId {
+			readers = append(readers, seg.NewReader())
+		}
+	}
+	if segId == 0 || w.activeSeg.id <= segId {
+		readers = append(readers, w.activeSeg.NewReader())
+	}
+
+	sort.Slice(readers, func(i, j int) bool {
+		return readers[i].seg.id < readers[j].seg.id
+	})
+
+	return &Reader{
+		readers: readers,
+		currIdx: 0,
+	}
+}
+
+// NewReaderWithLoc returns a new reader for WAL which only read
+// data from the given chunk location.
+func (w *WAL) NewReaderWithLoc(loc *ChunkLoc) (*Reader, error) {
+	if loc == nil {
+		return nil, errors.New("start location is nil")
+	}
+	w.mu.RLock()
+	defer w.mu.RUnlock()
+
+	reader := w.NewReader()
+	for {
+		// skip the segment whose id is less than loc.SegId
+		if reader.CurrSegId() < loc.SegId {
+			reader.Skip()
+			continue
+		}
+		// skip the chunk whose location is less than loc
+		curLoc := reader.CurrChunkLoc()
+		if curLoc.BlockIndex >= loc.BlockIndex &&
+			curLoc.ChunkOffset >= loc.ChunkOffset {
+			break
+		}
+		// call Next()
+		if _, _, err := reader.Next(); err != nil {
+			if err == io.EOF {
+				break
+			}
+			return nil, err
+		}
+	}
+
+	return reader, nil
+}
+
+// NewReader returns a new reader for WAL which read all segments.
+func (w *WAL) NewReader() *Reader {
+	return w.NewReaderLE(0)
+}
+
+// Skip skips the current segment.
+func (r *Reader) Skip() {
+	r.currIdx++
+}
+
+// CurrSegId returns the id of current segment.
+func (r *Reader) CurrSegId() SegmentID {
+	return r.readers[r.currIdx].seg.id
+}
+
+// CurrChunkLoc returns the location of current chunk.
+func (r *Reader) CurrChunkLoc() *ChunkLoc {
+	curReader := r.readers[r.currIdx]
+	return &ChunkLoc{
+		SegId:       curReader.seg.id,
+		BlockIndex:  curReader.blockIdx,
+		ChunkOffset: curReader.chunkOff,
+	}
+}
+
+// Next returns the next chunk data with location.
+// If there's no data, io.EOF will be returned.
+func (r *Reader) Next() ([]byte, *ChunkLoc, error) {
+	if r.currIdx >= len(r.readers) {
+		return nil, nil, io.EOF
+	}
+
+	data, loc, err := r.readers[r.currIdx].Next()
+	if err == io.EOF {
+		r.currIdx++
+		return r.Next()
+	}
+	return data, loc, err
+}
+
 // Close closes the WAL.
 func (w *WAL) Close() error {
 	w.mu.Lock()
@@ -220,6 +352,22 @@ func (w *WAL) Sync() error {
 	defer w.mu.Unlock()
 
 	return w.activeSeg.Sync()
+}
+
+// ActiveSegID returns the current active segment id.
+func (w *WAL) ActiveSegID() SegmentID {
+	w.mu.RLock()
+	defer w.mu.RUnlock()
+	return w.activeSeg.id
+}
+
+// IsEmpty returns whether the WAL is empty.
+// Only when there is only one active segment, and it's empty.
+func (w *WAL) IsEmpty() bool {
+	w.mu.RLock()
+	defer w.mu.RUnlock()
+
+	return len(w.olderSegs) == 0 && w.activeSeg.Size() == 0
 }
 
 func (w *WAL) isFull(delta int64) bool {
